3281a04d2142774fc0bbac649248dcd1a0646399
Rishav <rishavkumar08@gmail.com>
2018-03-18 21:59:43 -0500

    minor update

--- ML_4H.py
+++ ML_4H.py
@@ -1,26 +1,3 @@
-#####################################################################################################################
-#   CS 6375.003 - Assignment 3, Neural Network Programming
-#   This is a starter code in Python 3.6 for a 2-hidden-layer neural network.
-#   You need to have numpy and pandas installed before running this code.
-#   Below are the meaning of symbols:
-#   train - training dataset - can be a link to a URL or a local file
-#         - you can assume the last column will the label column
-#   train - test dataset - can be a link to a URL or a local file
-#         - you can assume the last column will the label column
-#   h1 - number of neurons in the first hidden layer
-#   h2 - number of neurons in the second hidden layer
-#   X - vector of features for each instance
-#   y - output for each instance
-#   w01, delta01, X01 - weights, updates and outputs for connection from layer 0 (input) to layer 1 (first hidden)
-#   w12, delata12, X12 - weights, updates and outputs for connection from layer 1 (first hidden) to layer 2 (second hidden)
-#   w23, delta23, X23 - weights, updates and outputs for connection from layer 2 (second hidden) to layer 3 (output layer)
-#
-#   You need to complete all TODO marked sections
-#   You are free to modify this code in any way you want, but need to mention it in the README file.
-#
-#####################################################################################################################
-
-
 import numpy as np
 import pandas as pd
 import sys
@@ -37,7 +14,6 @@ class NeuralNet:
         # h1 and h2 represent the number of nodes in 1st and 2nd hidden layers
 
         raw_input = pd.read_csv(train, header = None, na_values = [' ?','?','? ',' ? '], skip_blank_lines = True)
-        # TODO: Remember to implement the preprocess method
         train_dataset = self.preprocess(raw_input)
         ncols = len(train_dataset.columns)
         nrows = len(train_dataset.index)
@@ -96,9 +72,6 @@ class NeuralNet:
         self.X45 = np.zeros((len(self.X), h4))
         self.delta45 = np.zeros((h4, output_layer_size))
         self.deltaOut = np.zeros((output_layer_size, 1))
-    #
-    # TODO: I have coded the sigmoid activation function, you need to do the same for tanh and ReLu
-    #
 
     def __activation(self, x, activation="sigmoid"):
         if activation == "sigmoid":
@@ -108,9 +81,6 @@ class NeuralNet:
         elif activation == "tanh":
             self.__tanh(self, x)
 
-    #
-    # TODO: Define the function for tanh, ReLu and their derivatives
-    #
 
     def __activation_derivative(self, x, activation="sigmoid"):
         if activation == "sigmoid":
@@ -143,10 +113,7 @@ class NeuralNet:
     def __tanh_derivative(self, x):
         return (1 - x * x)
     
-    #
-    # TODO: Write code for pre-processing the dataset, which would include standardization, normalization,
-    #   categorical to numerical, etc
-    #
+ 
 
     def preprocess(self, X):
 #         fill missing values with the most frequent
@@ -248,7 +215,7 @@ class NeuralNet:
         self.compute_hidden_layer2_delta(activation)
         self.compute_hidden_layer1_delta(activation)
 
-    # TODO: Implement other activation functions
+
 
     def compute_output_delta(self, out, activation="sigmoid"):
         if activation == "sigmoid":
@@ -260,7 +227,6 @@ class NeuralNet:
 
         self.deltaOut = delta_output
 
-     # TODO: Implement other activation functions
     
     def compute_hidden_layer4_delta(self, activation="sigmoid"):
         if activation == "sigmoid":
@@ -272,7 +238,6 @@ class NeuralNet:
 
         self.delta45 = delta_hidden_layer4
         
-    # TODO: Implement other activation functions
 
     def compute_hidden_layer3_delta(self, activation="sigmoid"):
         if activation == "sigmoid":
@@ -284,7 +249,6 @@ class NeuralNet:
 
         self.delta34 = delta_hidden_layer3
         
-    # TODO: Implement other activation functions
 
     def compute_hidden_layer2_delta(self, activation="sigmoid"):
         if activation == "sigmoid":
@@ -296,7 +260,6 @@ class NeuralNet:
 
         self.delta23 = delta_hidden_layer2
 
-    # TODO: Implement other activation functions
 
     def compute_hidden_layer1_delta(self, activation="sigmoid"):
         if activation == "sigmoid":
@@ -308,7 +271,6 @@ class NeuralNet:
 
         self.delta12 = delta_hidden_layer1
 
-    # TODO: Implement other activation functions
 
     def compute_input_layer_delta(self, activation="sigmoid"):
         if activation == "sigmoid":
@@ -320,9 +282,7 @@ class NeuralNet:
 
         self.delta01 = delta_input_layer
 
-    # TODO: Implement the predict function for applying the trained model on the  test dataset.
-    # You can assume that the test dataset has the same format as the training dataset
-    # You have to output the test error from this function
+    # Assumed that the test dataset has the same format as the training dataset
 
     def predict(self, my_activation = "sigmoid", header = True):
         self.X = self.X_test
